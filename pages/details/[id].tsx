38adda3ce614d3b719b0eca5f5d26a2aeb615036
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-05-10 17:47:27 -0300

    Add poster and improve responsiveness

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -29,23 +29,37 @@ const DetailsScreen = ({ movie, tv }: Props) => {
         />
         <div className="absolute h-full w-full bg-gradient-to-t from-rose-800 to-black opacity-60 -z-10"></div>
       </div>
-      <div className="mx-5 absolute top-60 h-64 max-w-lg">
-        <div>
-          <h1 className="mb-2 text-white text-2xl">
-            {movie.title || movie.original_name}
-          </h1>
-          <p className="text-gray-200 mb-1 line-clamp-2">{movie.overview}</p>
-          <p className="mb-2">Action • Movie • 2022</p>
-        </div>
-        <div className="absolute bottom-0 w-full">
-          <button className=" mt-4 bg-white w-full py-2 rounded-md text-gray-700 font-bold tracking-wider text-sm transform hover:scale-105 transition-all duration-200">
-            START WATCHING: S1 E1
-          </button>
-          <div className="mt-5 flex items-center cursor-pointer hover:text-white transition-all duration-100">
-            <PlusCircleIcon className="w-auto h-7 block" />
-            <p className="text-[0.65rem] tracking-widest font-bold ml-1">
-              MY STUFF / RECORD
-            </p>
+      <div className="mx-5 absolute top-64 md:left-10 xl:left-24 md:top-20 max-w-2xl">
+        <div className="flex">
+          <div className="w-1/3 hidden md:block">
+            <Image
+              src={BASE_URL + tv.poster_path}
+              layout="responsive"
+              width={600}
+              height={1000}
+            />
+          </div>
+          <div className="md:ml-10 md:w-2/3 self-end">
+            <div>
+              <h1 className="mb-2 text-white text-2xl">
+                {movie.title || movie.original_name}
+              </h1>
+              <p className="text-gray-200 mb-1 line-clamp-2 md:line-clamp-4">
+                {movie.overview}
+              </p>
+              <p className="mb-2">Action • Movie • 2022</p>
+            </div>
+            <div className="w-full">
+              <button className=" mt-4 bg-white w-full py-2 rounded-md text-gray-700 font-bold tracking-wider text-sm transform hover:scale-105 transition-all duration-200">
+                START WATCHING: S1 E1
+              </button>
+              <div className="mt-5 flex items-center cursor-pointer hover:text-white transition-all duration-100">
+                <PlusCircleIcon className="w-auto h-7 block" />
+                <p className="text-[0.65rem] tracking-widest font-bold ml-1">
+                  MY STUFF / RECORD
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
